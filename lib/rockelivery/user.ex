df39f4cfcc2a1cee7ea359965f7ef241d2519ca6
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-06-23 23:36:13 -0300

    feat: POST users

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -8,13 +8,16 @@ defmodule Rockelivery.User do
 
   @required_params [:age, :address, :cep, :cpf, :email, :password, :name]
 
+  @derive {Jason.Encoder, only: [:id, :age, :cpf, :address, :email]}
+
   schema "users" do
     field :age, :integer
     field :address, :string
     field :cep, :string
     field :cpf, :string
     field :email, :string
-    field :password_hash, :string, virtual: true
+    field :password, :string, virtual: true
+    field :password_hash, :string
     field :name, :string
 
     timestamps()
@@ -24,7 +27,7 @@ defmodule Rockelivery.User do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
-    |> validate_length(:password_hash, min: 6)
+    |> validate_length(:password, min: 6)
     |> validate_length(:cep, is: 8)
     |> validate_length(:cpf, min: 11)
     |> validate_number(:age, greater_than_or_equal_to: 18)
@@ -34,7 +37,7 @@ defmodule Rockelivery.User do
     |> put_password_hash()
   end
 
-  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password} = changeset}) do
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
     change(changeset, Pbkdf2.add_hash(password))
   end
 
