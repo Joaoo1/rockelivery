fc5f0aaea2f3c08c3813517ec3fdde81ec202319
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-07-15 17:42:18 -0300

    ci: config test database

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,11 @@ config :rockelivery, Rockelivery.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+if System.get_env("GITHUB_ACTIONS") DO
+  config :rockelivery, Rockelivery.Repo,
+    username: "postgres",
+    password: "postgres",
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rockelivery, RockeliveryWeb.Endpoint,
