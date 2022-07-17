29a64a032ffdb6f3d0797bf45182a98afcfdf7f0
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-07-17 02:38:03 -0300

    build: remove runtime.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :rockelivery, Rockelivery.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -28,7 +28,8 @@ config :rockelivery, RockeliveryWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  server: true
 
 # ## Using releases (Elixir v1.9+)
 #
